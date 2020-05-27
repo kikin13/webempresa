3e712a749620b1965c54f34f9891adcb23ba36e5
Enrique Cervantes <cervtsluis13@gmail.com>
2020-05-27 09:07:08 -0500

    App social terminada

--- webempresa/settings.py
+++ webempresa/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'social.apps.SocialConfig',
     'core',
     'services.apps.ServicesConfig',
 ]
@@ -65,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social.processors.ctx_dict',
             ],
         },
     },
